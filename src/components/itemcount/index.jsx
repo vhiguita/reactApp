e1a88409f1b781d04d1bdf4a0ec140471738f45d
Victor Higuita <vhiguitacardona@gmail.com>
2021-02-02 19:37:32 -0500

    cambios desafío contador con Botón 2-02-2021

--- src/components/itemcount/index.jsx
+++ src/components/itemcount/index.jsx
@@ -3,45 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ItemCount = (props) =>{
   const [count, setCount] = useState(props.initial);
-  const [nStock, setStock] = useState(props.stock); //Stock disponible
+  let nStock = props.stock;
+  //const [nStock, setStock] = useState(props.stock); //Stock disponible
   function onDecrease(){
     if(count>1){
-      setCount(parseInt(count) - 1);
+      setCount(count - 1);
     }else {
       setCount(props.initial);
     }
   }
   function onIncrease(){
     if(count<nStock){
-      setCount(parseInt(count) + 1);
-    }
-  }
-  function addToCart(){
-    if(nStock!==0&&count<=nStock){
-      alert('agregando '+ count +' refs. al carrito.');
-      setStock(nStock - count);
-      //console.log(nStock);
-      setCount(props.initial);
+      setCount(count + 1);
     }
   }
 
   return (
      
     <>
-    {/* <div className="h1">{props.stock}</div>
-    <div className="h1">{props.initial}</div> */}
     <div style={{ padding: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '300px' }}>
     <button onClick={onDecrease}>-</button>
-    <input style={{width: '100px', display:'block', textAlign: 'center'}}
-            name="numberOfProducts"
-            type="text"
-            value={count}
-     />
+   
+    <p style={{width: '100px', display:'block', textAlign: 'center'}}>{count}</p>
     <button onClick={onIncrease}>+</button>
     
     </div>
     <div style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '300px'}}>
-      <div><button style={{width: '100px', height: '60px', minWidth: '200px', display:'block'}} onClick={addToCart}>Agregar al carrito</button>
+      <div><button style={{width: '100px', height: '60px', minWidth: '200px', display:'block'}} onClick={props.onAdd}>Agregar al carrito</button>
       </div>
     </div>
     
--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -2,11 +2,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemCount from './../components/itemcount/index';
 const ItemListContainer = ({greeting}) =>{
+  function onAdd(){
+    alert('agregando  al carrito.');
+  }
+
   return (
      
     <>
      <div className="h1">{greeting}</div>
-     <ItemCount stock="5" initial="1"/>
+     <ItemCount stock={5} initial={1} onAdd={onAdd}/>
     </>
    
   );
