1668ebf9abc4b0014b0aadfe027045f397094237
Victor Higuita <victorhiguita@MacBook-Pro-de-Victor.local>
2021-02-26 22:26:05 -0500

    cart view 26-02-2021

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -49,23 +49,41 @@ const Cart = () =>{
 
      <div>
        <ul>
-        {product.map((prod)=>{
-          return <div className='card'>
-                   <img src={prod.item.image}/>
-                   <h3 style={{textAlign: "center"}}>{prod.item.name}</h3>
-                   <p style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '300px'}}>Cantidad: {prod.quantity}</p>
-                   <div style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '300px'}}>
-                   <button style={{width: '100px', height: '60px', minWidth: '200px', display:'block'}} onClick={()=>{removeItemFromCart(prod.item.id)}}>Eliminar</button>
-                   </div>
-                  </div>
-        })}
+
        </ul>
+       <table id='product'>
+                <thead>
+                  <tr>
+                    <th>Cantidad</th>
+                    <th>Producto</th>
+                    <th>Precio Unit.</th>
+                    <th>Precio Total</th>
+                    <th>Imagen</th>
+                    <th></th>
+                   </tr>
+                </thead>
+                <tbody>
+                {product.map((prod)=>{
+                  return (
+                     <tr key={prod.item.image.id}>
+                         <td>{prod.quantity}</td>
+                         <td>{prod.item.name}</td>
+                         <td>{formatter.format(prod.item.price)}</td>
+                         <td>{formatter.format(prod.item.price * prod.quantity)}</td>
+                         <td><img className="image" src={prod.item.image}/></td>
+                         <td className='opration'>
+                             <button onClick={()=>{removeItemFromCart(prod.item.id)}}>Eliminar</button>
+                         </td>
+                     </tr>
+                  )})}
+                </tbody>
+        </table>
      </div>
      {isCartEmpty ?
        <>
        </>
        :<div style={{ textAlign: 'center', alignItems: 'center', justifyContent: 'center', width: '300px'}}>
-         <strong>PRECIO TOTAL = </strong> {formatter.format(totalPrice)}
+         <strong>TOTAL = </strong> {formatter.format(totalPrice)}
         </div>
      }
     </>
